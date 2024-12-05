bd6a5b5559b739f9a7907f3aa1c2c06cdebbb0e2
piyushxyz1 <piyushnegi033@gmail.com>
2024-12-05 13:14:55 +0530

    changes made

--- assets/index.js
+++ assets/index.js
@@ -337,6 +337,63 @@ rightArrow4.addEventListener('click', () => {
 
 
 
+$('.hotels_array3').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+
+const hotelsandresorts3 = document.querySelector('.hotels_array3 div');
+const leftArrow5 = document.querySelector('.left-arrow5');
+const rightArrow5 = document.querySelector('.right-arrow5');
+
+
+leftArrow5.addEventListener('click', () => {
+  hotelsandresorts3.scrollBy({
+    left: -300, // Scroll left by 300px
+    behavior: 'smooth', // Smooth scrolling
+  });
+});
+
+rightArrow5.addEventListener('click', () => {
+  hotelsandresorts3.scrollBy({
+    left: 300, // Scroll right by 300px
+    behavior: 'smooth', // Smooth scrolling
+  });
+});
 
 
 

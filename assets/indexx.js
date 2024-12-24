a09e21545bfa71827ed759d5f293b3fb0bfab690
piyushxyz1 <piyushnegi033@gmail.com>
2024-12-24 13:28:06 +0530

    changes made

--- assets/indexx.js
+++ assets/indexx.js
@@ -128,3 +128,22 @@ function validateForm() {
 
 
 
+
+
+
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
